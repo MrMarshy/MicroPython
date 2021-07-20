61a505194b8ec4ffaa7f54ff5a0b8060eb13e98d
MrMarshy <mrmarshy1981@gmail.com>
2021-07-20 13:53:29 +0100

    add pi pico blinky example

--- PiPico/blinky.py
+++ PiPico/blinky.py
@@ -1,13 +1,13 @@
 from machine import Pin
 from time import sleep_ms
 
-led = Pin(2, Pin.OUT)
+led = Pin(25, Pin.OUT)
 
 while True:
     led.value(1)
-    sleep_ms(1000)
+    sleep_ms(500)
     led.value(0)
-    sleep_ms(1000)
+    sleep_ms(500)
     
     
     
